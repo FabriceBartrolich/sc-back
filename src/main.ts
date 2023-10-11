5fb862ac854d9bb9d27c30d3c043f35566924ea9
FabriceBartrolich <fabrice.bartrolich@sncf.fr>
2023-10-11 14:29:11 +0200

    feat: Ajouter les dépendances essentielles de Nest.js [back]

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+app.useGlobalPipes(new ValidationPipe());
+
+const config = new DocumentBuilder()
+    .setTitle('Series Connect API')
+    .setDescription('The Series Connect API description')
+    .setVersion('1.0')
+    .addTag('Series-Connect')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
